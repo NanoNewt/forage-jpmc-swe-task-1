02b6494be4dca1b0ec5d731133bdd9857031706f
NanoNewt <quinn@nicodemus.name>
2023-11-27 15:42:32 -0700

    updated getRatio

--- client3.py
+++ client3.py
@@ -40,7 +40,9 @@ def getDataPoint(quote):
 
 def getRatio(price_a, price_b):
     """ Get ratio of price_a and price_b """
-    return 1
+    if price_b == 0: return
+
+    return price_a/price_b
 
 
 # Main
