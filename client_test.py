f695406c5212b1f60e38fadea0aab5e5773776de
NanoNewt <quinn@nicodemus.name>
2023-12-21 18:45:47 -0700

    unit_tests

--- client_test.py
+++ client_test.py
@@ -1,5 +1,6 @@
 import unittest
 from client3 import getDataPoint
+from client3 import getRatio
 
 class ClientTest(unittest.TestCase):
   def test_getDataPoint_calculatePrice(self):
@@ -8,6 +9,15 @@ class ClientTest(unittest.TestCase):
       {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
     ]
     """ ------------ Add the assertion below ------------ """
+    for quote in quotes:
+       expected_tuple = (
+          quote['stock'],
+          quote['top_bid']['price'],
+          quote['top_ask']['price'],
+          (quote['top_bid']['price']+quote['top_ask']['price'])/2
+        )
+       self.assertEqual(getDataPoint(quote),expected_tuple)
+
 
   def test_getDataPoint_calculatePriceBidGreaterThanAsk(self):
     quotes = [
@@ -15,10 +25,18 @@ class ClientTest(unittest.TestCase):
       {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
     ]
     """ ------------ Add the assertion below ------------ """
-
+    for quote in quotes:
+       expected_tuple = (
+          quote['stock'],
+          quote['top_bid']['price'],
+          quote['top_ask']['price'],
+          (quote['top_bid']['price']+quote['top_ask']['price'])/2
+        )
+       self.assertEqual(getDataPoint(quote),expected_tuple)
 
   """ ------------ Add more unit tests ------------ """
-
+  def test_getRatio_A_zero(self):
+    self.assertEqual(getRatio(0,100),0)
 
 
 if __name__ == '__main__':
